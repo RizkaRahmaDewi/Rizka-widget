aa8dc282a74247c644b6cd3c2710a21350bb1201
RizkaRahmaDewi <131003139+RizkaRahmaDewi@users.noreply.github.com>
2023-07-06 16:27:40 +0700

    first commit

---
+++ src/views/foto.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="photo-widget">
+        <h1 class="ket">Wallpaper Pemandangan</h1>
+      <div class="image-container">
+        <img :src="currentPhoto" :alt="currentPhoto" :style="{ width: '1000px', height: 'auto' }" />
+      </div>
+      <div class="controls-foto">
+        <button @click="BackToHome">Back To Home</button>
+        <button @click="prevPhoto">Previous</button>
+        <button @click="nextPhoto">Next</button>
+      </div>
+    </div>
+  </template>
+  
+  
+  <script>
+  export default {
+    data() {
+      return {
+        photos: [require('../assets/gambar1.jpg'),require('../assets/gambar2.jpg'),require('../assets/gambar3.jpg'),require('../assets/gambar4.jpg'),require('../assets/gambar5.jpg'),require('../assets/gambar6.jpg'),require('../assets/gambar7.jpg'),require('../assets/gambar8.jpg'),require('../assets/gambar9.jpg'),require('../assets/gambar10.jpg')],
+        currentIndex: 0,
+      };
+    },
+    computed: {
+      currentPhoto() {
+        return this.photos[this.currentIndex];
+      },
+    },
+    methods: {
+      BackToHome() {
+        this.$router.push('/home')
+      },
+      prevPhoto() {
+        if (this.currentIndex > 0) {
+          this.currentIndex--;
+        } else {
+          this.currentIndex = this.photos.length - 1;
+        }
+      },
+      nextPhoto() {
+        if (this.currentIndex < this.photos.length - 1) {
+          this.currentIndex++;
+        } else {
+          this.currentIndex = 0;
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style>
+.ket {
+    font-weight: bold;
+    margin-bottom: 50px;
+}
+
+.photo-widget {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.image-container {
+  margin-bottom: 20px;
+}
+
+.controls-foto button {
+  padding: 10px 20px;
+  font-size: 18px;
+  background-color:#d05eb6;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  border-radius: 50px;
+  margin-right: 10px;
+}
+
+.controls-foto button:hover {
+  background-color:#830c83
+}
+
+  </style>
